241fa8dda1c90af7e790f918e02c6aee9a0fe17a
JoyWanddrr <m81y102@msn.com>
2022-10-13 16:19:38 +0800

    refactor:modify express router

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,23 @@
 // 總路由，記得根據RESTful改寫app.js的路由edit，delete
 // 記得安裝npm install method-override@3.0.0
+
+
+const express = require('express')
+const router = express.Router()
+
+// 引入home模組程式碼(router)
+const home = require('./modules/home')
+// 引入restaurants模組(router)
+const restaurant = require('./modules/restaurants')
+// 將網址結構符合 / 字串的 request 導向 home 模組(如果request路徑是'/',就執行home的程式碼) 。此為首頁路由。最後再放入app.js使用。
+router.use('/', home)
+// restaurants路由
+router.use('/restaurants', restaurant)
+
+
+
+
+
+
+// 匯出路由器，這裡的router是const router嗎
+module.exports = router
---
+++ routes/modules/home.js
@@ -0,0 +1,35 @@
+// 首頁路由，home page
+
+const express = require('express')
+const router = express.Router
+// 載入restaurants的schema
+const Restaurant = require('./../../models/restaurant')
+
+router.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
+})
+
+// 搜尋特定餐廳
+router.get("/search", (req, res) => {
+  // 擷取input
+  const keywords = req.query.keyword.trim()
+  // 如果查詢不到則返回首頁
+  if (!keywords) {
+    res.redirect("/")
+  }
+  Restaurant.find({})
+    .lean()
+    .then(restaurants => {
+      const filterRestaurant = restaurants.filter(
+        (data) =>
+          data.name.toLowerCase().includes(keywords) ||
+          data.category.includes(keywords)
+      )
+      res.render('index', { restaurants: filterRestaurant, keywords })
+    })
+    .catch(err => console.log(err))
+})
+module.export = router
